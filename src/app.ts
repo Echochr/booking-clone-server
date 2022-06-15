79d09a3ab5210a46410d950159ed6f717900982f
Chris Ho <chris.ho@technet-asia.com>
2022-06-15 11:03:06 +0800

    feat(app): install morgan for http requests logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 
 import v1Router from './routes/v1';
 
+dotenv.config();
+
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 // Routes
