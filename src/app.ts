98425cdbf09c7d01bdd869f4c98bb38d26760795
Chris Ho <chris.ho@technet-asia.com>
2022-07-03 19:00:56 +0800

    feat(app): add helmet to app configuration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 
 import v1Router from './routes/v1';
@@ -13,6 +14,7 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
+app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
