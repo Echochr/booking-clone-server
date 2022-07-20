765b06488ef4ddc5733e99e2f309e4b52154f168
Chris Ho <chris.ho@technet-asia.com>
2022-07-20 17:58:39 +0800

    fix: access_token not set to browser using axios from frontend client

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000' || 'https://booking-clone-client.netlify.app/',
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json());
 
