ddd67e351cfe828cda7fefdd53c439205623e048
Chris Ho <chris.ho@technet-asia.com>
2022-07-21 16:26:21 +0800

    fix(app): cors origin

--- src/routes/v1/auth/auth.controller.ts
+++ src/routes/v1/auth/auth.controller.ts
@@ -52,6 +52,8 @@ export async function httpLoginUser(req: Request, res: Response, next: NextFunct
           .cookie('access_token', token, {
             maxAge: 60 * 60 * 1000, // 1 hour
             httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production',
           })
           .status(200).json({ id, isAdmin });
       }
